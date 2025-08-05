865c1567bab2efc92ff43bdf8924f3ff3793081e
vladluca70 <vladluca70@gmail.com>
2025-08-05 20:38:05 +0300

    authentication page init+ server init

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
 
 
 function AuthPage()
 {
+    const [username, setUsername]=useState('')
+    const [errorMessage, setErrorMessage]=useState('')
 
+    function handleChangeName(e){
+        setUsername(e.target.value)
+    }
+
+    async function handleLoginButton(){
+        await handleRequest('login')
+    }
+
+    async function handleRequest(requestType)
+    {
+        const url=`http://localhost:5000/${requestType}`
+
+        try {
+
+        } 
+        catch (error) {
+
+            console.error("errors", error)
+        }
+    }
+
+    return(
+        <>
+            <input onChange={(e)=>handleChangeName(e)}/>
+            <button onClick={handleLoginButton}>Log in</button>
+        </>
+    )
 }
 
 export default AuthPage;
