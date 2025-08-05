0beaf5d4e2fd3d77c043276b2faaff35a93c72ff
vladluca70 <vladluca70@gmail.com>
2025-08-05 20:55:26 +0300

    database connection

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,29 @@ const cors=require('cors')
 const mongoose=require('mongoose')
 
 const port=5000
+const mongoURL='mongodb://localhost:27017/testNOTES'
 const server=express()
 
 server.use(express.json())
 server.use(cors())
 
+mongoose.connect(mongoURL, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("database connection established")
+}).catch((error)=>{
+    console.log("database connection error", error)
+})
+
+const noteSchema=new mongoose.Schema({
+    name:{type:String, required:true},
+    note:{type:String}
+})
+const noteModel=mongoose.model('Note', noteSchema)
+
 server.post('/login', async(req, res)=>{
-    
+
 })
 
 server.listen(
