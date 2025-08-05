41a8cea02320c7ded39f1c635a5e5a91129e1c73
vladluca70 <vladluca70@gmail.com>
2025-08-05 20:24:13 +0300

    welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import WelcomePage from './WelcomePage';
+import AuthPage from './AuthPage';
 
 function App() {
-    
+    const [username, setUsername]=useState('')
+
+    if(username)
+    {
+      <WelcomePage username={username}/>
+    }
+    else
+    {
+      <AuthPage/>
+    }
 }
 
 export default App;
--- frontend/src/WelcomePage.jsx
+++ frontend/src/WelcomePage.jsx
@@ -1,8 +1,12 @@
 
 
-function WelcomePage()
+function WelcomePage({username})
 {
-
+    return(
+        <>
+        <p>Welcome {username}</p>
+        </>
+    )
 }
 
 export default WelcomePage;
